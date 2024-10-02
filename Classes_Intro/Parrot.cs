bbcbcbdde647ab1af207d733126c5da5a80f5e79
Mstyslav Sakidon <mstislavs@devart.com>
2024-10-02 16:35:27 +0300

    Add project files.

---
+++ Classes_Intro/Parrot.cs
@@ -0,0 +1,34 @@
+﻿namespace Classes_Intro
+{
+    internal class Parrot
+    {
+        private string _name;
+        private string _ownerName;
+
+        public Parrot(string name, string ownerName)
+        {
+            _name = name;
+            _ownerName = ownerName;
+        }
+
+        public Parrot()
+        { }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string OwnerName
+        {
+            get { return _ownerName; }
+            set { _ownerName = value; }
+        }
+
+        public string SayHello(Parrot parrot)
+        {
+            return ($"zin, i'am parrot, my name is: {parrot.Name}");
+        }
+    }
+}
---
+++ Classes_Intro/Program.cs
@@ -0,0 +1,8 @@
+﻿using Classes_Intro;
+
+Parrot parrot = new Parrot();
+parrot.Name = "Denny";
+parrot.OwnerName = "Sofia";
+Console.WriteLine(parrot.SayHello(parrot));   
+Parrot parrot2 = new Parrot("Kesha", "Alina");
+Console.WriteLine(parrot.SayHello(parrot2));
